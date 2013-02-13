fb91a766fa6dc79236ec46dbedb389eb2b3d3b91
Nicolas Georget <nicolas.georget@gmail.com>
2013-02-13 11:42:55 +0700

    initial commit
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ SandboxCLI/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  SandboxCLI
+//
+//  Created by Nicolas Georget on 2/13/13.
+//  Copyright (c) 2013 Nicolas Georget. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      
+    // insert code here...
+    NSLog(@"Hello, World!");
+      
+  }
+  return 0;
+}
+

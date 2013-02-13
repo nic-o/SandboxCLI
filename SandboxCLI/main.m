4ff2bf341ccf00f18b8e9f941bc638037a9af712
Nicolas Georget <nicolas.georget@gmail.com>
2013-02-13 11:48:51 +0700

    Type of volume mounted (SMB, AppleShare, etc…)
    How to get the type of mounted volume with NSURLVolumeLocalizedFormatDescriptionKey
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

--- SandboxCLI/main.m
+++ SandboxCLI/main.m
@@ -11,8 +11,26 @@
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
       
-    // insert code here...
-    NSLog(@"Hello, World!");
+    // http://stackoverflow.com/questions/7828783/cocoa-how-to-get-the-already-mounted-unmountable-disk
+    
+    NSArray *keys = [NSArray arrayWithObjects:NSURLVolumeNameKey, NSURLVolumeIsRemovableKey, nil];
+    NSArray *urls = [[NSFileManager defaultManager]
+                     mountedVolumeURLsIncludingResourceValuesForKeys:keys
+                     options:NSVolumeEnumerationSkipHiddenVolumes];
+    
+    NSLog(@"%@", urls);
+    for (NSURL *url in urls) {
+      NSError *error;
+      //      NSNumber *isRemovable;
+      NSString *volumeName;
+      //      [url getResourceValue:&isRemovable forKey:NSURLVolumeIsRemovableKey error:&error];
+      //      if ([isRemovable boolValue]) {
+      //[url getResourceValue:&volumeName forKey:NSURLVolumeNameKey error:&error];
+      [url getResourceValue:&volumeName forKey:NSURLVolumeLocalizedFormatDescriptionKey error:&error];
+      NSLog(@"%@", volumeName);
+      //      }
+    }
+
       
   }
   return 0;
